fa55dc153a5ba5079a792d7d0c09810c54875c85
SShankar-ssh <sofieshankar@gmail.com>
2025-02-13 21:42:56 +0100

    Update Christmas_Tree.R

--- Christmas_Tree.R
+++ Christmas_Tree.R
@@ -1,1 +1,9 @@
 source: https://www.linkedin.com/posts/alexander-krannich_rstats-rstudio-r-activity-7277239219834503170-kD5d?utm_source=share&utm_medium=member_desktop
+xmastreeshape(
+  year = 2026,
+  language = "spanish",
+  shape = "piramidal",
+  nballs = 50,
+  ballscolor = c("white", "orange2", "red","magenta4","yellow"),
+
+)
