5015b5c6d8ea6abde3d9af4643a04942717fda09
SShankar-ssh <sofieshankar@gmail.com>
2025-06-03 17:21:11 +0200

    Add some standard set up functions as git_vaccinat and git_strep.

--- update_r_version_packages.R
+++ update_r_version_packages.R
@@ -1,2 +1,8 @@
 R.version.string
 update.packages(ask = FALSE, checkBuilt = TRUE)
+
+#Check that your user name and email are defined. If not, run
+usethis::git_sitrep()
+
+#It’s also a good idea to vaccinate. This implements best practice by adding files to your global .gitignore:
+usethis::git_vaccinate() 
