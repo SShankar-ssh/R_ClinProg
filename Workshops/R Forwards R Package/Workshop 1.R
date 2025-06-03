046b27b5053886db1a8ce32d533cde2f3f351021
SShankar-ssh <sofieshankar@gmail.com>
2025-06-03 17:36:37 +0200

    Initial commit

--- Workshops/R Forwards R Package/Workshop 1.R
+++ Workshops/R Forwards R Package/Workshop 1.R
@@ -6,3 +6,7 @@ usethis::git_sitrep()
 usethis::git_vaccinate() 
 #Create package animalsounds
 usethis::create_package("C:/Users/sonak/ClinProg_R/Workshops/R Forwards R Package/animalsounds")
+
+#Make your package a git repo( create a .Rproj files)
+usethis::use_git()
+  
