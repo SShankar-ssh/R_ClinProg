df2afb28b61973a8d9edfbb1bd67433bd5e6a4f6
SShankar-ssh <sofieshankar@gmail.com>
2025-06-17 20:42:56 +0200

    RClinprog renamed not a directory anymore

--- update_r_version_packages.R
+++ update_r_version_packages.R
@@ -6,3 +6,9 @@ usethis::git_sitrep()
 
 #It’s also a good idea to vaccinate. This implements best practice by adding files to your global .gitignore:
 usethis::git_vaccinate() 
+
+## install if needed (do this exactly once):
+## install.packages("usethis")
+
+#library(usethis)
+#use_git_config(user.name = "Jane Doe", user.email = "jane@example.org")
