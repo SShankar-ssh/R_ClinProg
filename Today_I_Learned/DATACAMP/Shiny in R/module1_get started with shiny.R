f82789877c4e0a6a5470451b0b8866a67286b8c5
SShankar-ssh <sofieshankar@gmail.com>
2026-02-19 00:11:16 +0100

    Upload code final assignment of Module 1

---
+++ Today_I_Learned/DATACAMP/Shiny in R/module1_get started with shiny.R
@@ -0,0 +1,30 @@
+source("setup.R")
+
+pkgs <- c("shiny", "ggplot2", "readxl")
+install_and_load(pkgs)
+
+# Read the Excel file
+babynames <- read_excel("C:/Users/shankaso/OneDrive - Danone/Documents/Training/DATACAMP/Building Web Applications with Shiny/Data/babynames.xlsx")
+
+
+ui <- fluidPage(
+  titlePanel("Baby Name Explorer"),
+  sidebarLayout(
+    sidebarPanel(textInput('name', 'Enter Name', 'David')),
+    mainPanel(plotOutput('trend'))
+  )
+)
+
+server <- function(input, output, session) {
+  output$trend <- renderPlot({
+    data_name <- subset(
+      babynames, name == input$name
+    )
+    # CODE BELOW: Update to display a line plot of the input name
+    ggplot(subset(babynames, name == input$name)) + 
+      geom_line(aes(x = year, y = prop, color = sex))
+    
+  })
+}
+
+shinyApp(ui = ui, server = server)
