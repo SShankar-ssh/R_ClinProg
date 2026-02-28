c77a5c9c59934d4c5b57e89c000f8d9c4ed209d1
SShankar-ssh <sofieshankar@gmail.com>
2026-02-28 19:15:56 +0100

    Clean up update_r_version_packages.R
    Removed comments and unused code related to user configuration and package installation.

--- update_r_version_packages.R
+++ update_r_version_packages.R
@@ -2,18 +2,3 @@
 #It will prompt you for each package that can be upgraded. If you want it to upgrade all packages without asking, use ask = FALSE:
 update.packages(ask = FALSE)
 update.packages(ask = FALSE, checkBuilt = TRUE)
-
-#Check that your user name and email are defined. If not, run
-usethis::git_sitrep()
-
-#It’s also a good idea to vaccinate. This implements best practice by adding files to your global .gitignore:
-usethis::git_vaccinate() 
-
-## install if needed (do this exactly once):
-## install.packages("usethis")
-
-#library(usethis)
-#use_git_config(user.name = "Jane Doe", user.email = "jane@example.org")
-
-#updating an expired token
-https://forum.posit.co/t/how-to-i-use-a-new-github-pat/112700
