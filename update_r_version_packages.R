b2e193939ceea4b00fdb5cfe849ebed65f576233
SShankar-ssh <sofieshankar@gmail.com>
2026-03-01 03:43:54 +0100

    Enhance R version update script
    Added functionality to check and update R version and packages.

--- update_r_version_packages.R
+++ update_r_version_packages.R
@@ -1,4 +1,19 @@
-#R.version.string
+# Check which R version is installed
+getRversion()
+
+# Update R version to the latest R version
+# install.packages("installr")
+library(installr)
+updateR()
+
+#Move the packages from your old R version to the new one
+#Update the moved packages
 #It will prompt you for each package that can be upgraded. If you want it to upgrade all packages without asking, use ask = FALSE:
 update.packages(ask = FALSE)
 update.packages(ask = FALSE, checkBuilt = TRUE)
+
+#Verify that all went well
+version()
+packageStatus()
+
+
